1a0635400bd5cb041a220aad4dc7454ec220d003
WalterNights <walternights@gmail.com>
2025-08-20 10:55:45 -0500

    spread the sidebar and navbar on individual components in dashboard view

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -2,18 +2,25 @@ from users.models import *
 from rest_framework import status
 from django.shortcuts import render
 from rest_framework.views import APIView
-from users.serializers import UserProfileSerializer
 from rest_framework.response import Response
+from users.serializers import UserProfileSerializer
 from rest_framework.permissions import IsAuthenticated
 
+
 class dashboardUserList(APIView):
+    permission_classes = [IsAuthenticated]
     
     def get(self, request):
         users = UserProfile.objects.all()
-        
-        for user in users:
-            print(user.user.email)
-        
         serializer = UserProfileSerializer(users, many=True)
-        
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+class dashboardUserData(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        profile = request.user.profile
+        serializer = UserProfileSerializer(profile)
+        return Response(serializer.daya, status=status.HTTP_200_OK)
+        
